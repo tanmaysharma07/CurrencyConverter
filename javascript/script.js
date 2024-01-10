7180bdd08cfc1abf67f610f7b274b167c0d26706
tanmaysharma07 <tanmaysharma679@gmail.com>
2024-01-11 00:14:42 +0530

    Api added and more options added

--- javascript/script.js
+++ javascript/script.js
@@ -13,31 +13,53 @@ currency_to.addEventListener("change",function(){
 })
 
 convert_button.addEventListener("click",function(){
-    let amount = document.getElementById("amount_input").value;
+    let amount = document.getElementById("amount_input");
+    amount.value = parseFloat(amount.value).toFixed(2);
+    amount = amount.value;
+    let base_currency = currency_from.value.toLowerCase();
+    let base_to_currency = currency_to.value.toLowerCase();
+
+    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${base_currency}.json`)
+
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    })
+    .then((json) => {
+        let amount_multiple = json[base_currency][base_to_currency];
+        amount *= amount_multiple;
+        currency_amount.innerHTML = `${base_to_currency.toUpperCase()} ${amount}`;
+    })
+    .catch()
+
+
+
     // window.alert(amount);
 
-    switch(true){
-        case (currency_from.value == "INR" && currency_to.value == "INR") : 
-        currency_amount.innerHTML = `INR ${amount}`;
-        break;
+    // switch(true){
+    //     case (currency_from.value == "INR" && currency_to.value == "INR") : 
+    //     currency_amount.innerHTML = `INR ${amount}`;
+    //     break;
 
-        case (currency_from.value == "INR" && currency_to.value == "USD") : 
-        amount = amount*.012;
-        currency_amount.innerHTML = `USD ${amount}`;
-        break;
+    //     case (currency_from.value == "INR" && currency_to.value == "USD") : 
+    //     amount = amount*.012;
+    //     currency_amount.innerHTML = `USD ${amount}`;
+    //     break;
 
-        case (currency_from.value == "USD" && currency_to.value == "INR") : 
-        amount = amount*83.11;
-        currency_amount.innerHTML = `INR ${amount}`;
-        break;
+    //     case (currency_from.value == "USD" && currency_to.value == "INR") : 
+    //     amount = amount*83.11;
+    //     currency_amount.innerHTML = `INR ${amount}`;
+    //     break;
 
-        case (currency_from.value == "USD" && currency_to.value == "USD") : 
-        currency_amount.innerHTML = `USD ${amount}`;
-        break;
+    //     case (currency_from.value == "USD" && currency_to.value == "USD") : 
+    //     currency_amount.innerHTML = `USD ${amount}`;
+    //     break;
 
-        default : 
-        currency_amount.innerHTML = `${currency_from.value} to ${currency_to.value}`
+    //     default : 
+    //     currency_amount.innerHTML = `${currency_from.value} to ${currency_to.value}`
 
-    }
+    // }
 
 });
